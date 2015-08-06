0c214814437be6e572f7dbf1fd1aaa4c67f091a7
Sergey Kharchenko <sergey.n.kharchenko@gmail.com>
2015-08-06 18:00:11 +0300

    Fix Users repository

--- src/main/java/com/home/testspring/repositories/Users.java
+++ src/main/java/com/home/testspring/repositories/Users.java
@@ -3,5 +3,5 @@ package com.home.testspring.repositories;
 import com.home.testspring.beans.User;
 
 public interface Users extends AbstractRepository<User> {
-    User getUserByName(String userName);
+    User getUserByName(String name);
 }
--- src/main/java/com/home/testspring/repositories/impl/UsersImpl.java
+++ src/main/java/com/home/testspring/repositories/impl/UsersImpl.java
@@ -3,8 +3,6 @@ package com.home.testspring.repositories.impl;
 import com.home.testspring.beans.User;
 import com.home.testspring.repositories.Users;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class UsersImpl extends AbstractRepositoryImpl<User> implements Users {
@@ -13,9 +11,9 @@ public class UsersImpl extends AbstractRepositoryImpl<User> implements Users {
         return User.class;
     }
 
-    public User getUserByName(String userName) {
+    public User getUserByName(String name) {
         return em.createQuery("SELECT u FROM User u WHERE username=:username", User.class)
-                .setParameter("username", userName)
+                .setParameter("username", name)
                 .setMaxResults(1)
                 .getSingleResult();
     }
